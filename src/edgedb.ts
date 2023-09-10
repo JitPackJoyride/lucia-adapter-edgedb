c28e59b4c161421ed874a9a4a214652d8832b8d7
JitPackJoyride <ajitkrishna99@gmail.com>
2023-09-10 16:03:15 +0100

    feat: :sparkles: implemented all the functions needed for the adapter!

--- src/edgedb.ts
+++ src/edgedb.ts
@@ -1,6 +1,7 @@
 import type { Client } from "edgedb";
 import type {
   Adapter,
+  GlobalDatabaseSessionAttributes,
   GlobalDatabaseUserAttributes,
   InitializeAdapter,
 } from "lucia";
@@ -38,7 +39,7 @@ export const edgedbAdapter = (
           User,
           (userObj: GlobalDatabaseUserAttributes) => ({
             ...User["*"],
-            filter_single: e.op(userObj.id, "=", userId),
+            filter_single: e.op(userObj.id, "=", e.cast(e.uuid, userId)),
           })
         );
 
@@ -65,25 +66,18 @@ export const edgedbAdapter = (
             const userInsertQuery = e.insert(User, user);
             const result = await userInsertQuery.run(tx);
 
-            // Removing user_id from key object
-            const {
-              hashed_password: hashedPassword,
-              id: keyId,
-              user_id,
-              ...rest
-            } = key;
-
+            // Removing user_id from key object and changing id to key_id
+            const { id: key_id, user_id, ...keyWithoutUserId } = key;
             const newKey = {
-              ...rest,
-              hashedPassword,
-              keyId,
+              key_id,
+              ...keyWithoutUserId,
             };
 
             const keyInsertQuery = e.insert(Key, {
               ...newKey,
-              user: e.select(User, (userObj: GlobalDatabaseUserAttributes) => {
-                filter_single: e.op(userObj.id, "=", e.cast(e.uuid, result.id));
-              }),
+              user: e.select(User, (userObj: GlobalDatabaseUserAttributes) => ({
+                filter_single: e.op(userObj.id, "=", e.cast(e.uuid, result.id)),
+              })),
             });
             await keyInsertQuery.run(tx);
           });
@@ -93,7 +87,7 @@ export const edgedbAdapter = (
           if (
             (error as ErrorCasting).message &&
             (error as ErrorCasting).message?.includes(
-              `${modelNames.key}: keyId`
+              `${modelNames.key}: key_id`
             )
           ) {
             throw new LuciaError("AUTH_DUPLICATE_KEY_ID");
@@ -102,15 +96,61 @@ export const edgedbAdapter = (
           throw error;
         }
       },
-      deleteUser: async (userId) => {},
-      updateUser: async (userId, partialUser) => {},
+      deleteUser: async (userId) => {
+        const query = e.delete(
+          User,
+          (userObj: GlobalDatabaseUserAttributes) => ({
+            filter_single: e.op(userObj.id, "=", e.cast(e.uuid, userId)),
+          })
+        );
+        await query.run(client);
+      },
+      updateUser: async (userId, partialUser) => {
+        const query = e.update(
+          User,
+          (userObj: GlobalDatabaseUserAttributes) => ({
+            filter_single: e.op(userObj.id, "=", e.cast(e.uuid, userId)),
+            set: partialUser,
+          })
+        );
+
+        await query.run(client);
+      },
       getSession: async (sessionId) => {
-        return null;
+        if (Session === null) {
+          throw new Error("Session table not defined");
+        }
+
+        const query = e.select(
+          Session,
+          (sessionObj: GlobalDatabaseSessionAttributes) => ({
+            ...Session["*"],
+            filter_single: e.op(sessionObj.id, "=", e.cast(e.uuid, sessionId)),
+          })
+        );
+
+        return await query.run(client);
       },
       getSessionsByUserId: async (userId) => {
-        return [];
+        if (Session === null) {
+          throw new Error("Session table not defined");
+        }
+
+        const query = e.select(
+          Session,
+          (sessionObj: GlobalDatabaseSessionAttributes) => ({
+            ...Session["*"],
+            filter: e.op(sessionObj.user.id, "=", e.cast(e.uuid, userId)),
+          })
+        );
+
+        return await query.run(client);
       },
       setSession: async (session) => {
+        if (Session === null) {
+          throw new Error("Session table not defined");
+        }
+
         if (uuidValidate(session.id) === false) {
           // In EdgeDB, the id should always be a UUID
           // If it's not, we need to delete it, so that EdgeDB can generate a new one
@@ -118,207 +158,173 @@ export const edgedbAdapter = (
 
           delete session.id;
         }
+
+        try {
+          const { user_id, ...sessionWithoutUserId } = session;
+          const query = e.insert(Session, {
+            ...sessionWithoutUserId,
+            user: e.select(User, (userObj: GlobalDatabaseUserAttributes) => ({
+              filter_single: e.op(userObj.id, "=", e.cast(e.uuid, user_id)),
+            })),
+          });
+          await query.run(client);
+        } catch (error) {
+          // Catch invalid user id errors
+          type ErrorCasting = { message?: string };
+
+          // Catch invalid user id errors
+          if (
+            (error as ErrorCasting).message &&
+            (error as ErrorCasting).message?.includes(
+              `missing value for required link 'user'`
+            )
+          ) {
+            throw new LuciaError("AUTH_INVALID_USER_ID");
+          }
+
+          throw error;
+        }
       },
-      deleteSession: async (sessionId) => {},
-      deleteSessionsByUserId: async (userId) => {},
-      updateSession: async (sessionId, partialSession) => {},
+      deleteSession: async (sessionId) => {
+        if (Session === null) {
+          throw new Error("Session table not defined");
+        }
+
+        const query = e.delete(
+          Session,
+          (sessionObj: GlobalDatabaseSessionAttributes) => ({
+            filter_single: e.op(sessionObj.id, "=", e.cast(e.uuid, sessionId)),
+          })
+        );
+
+        await query.run(client);
+      },
+      deleteSessionsByUserId: async (userId) => {
+        if (Session === null) {
+          throw new Error("Session table not defined");
+        }
+
+        const query = e.delete(
+          Session,
+          (sessionObj: GlobalDatabaseSessionAttributes) => ({
+            filter: e.op(sessionObj.user.id, "=", e.cast(e.uuid, userId)),
+          })
+        );
+
+        await query.run(client);
+      },
+      updateSession: async (sessionId, partialSession) => {
+        if (Session === null) {
+          throw new Error("Session table not defined");
+        }
+
+        const query = e.update(
+          Session,
+          (sessionObj: GlobalDatabaseSessionAttributes) => ({
+            filter_single: e.op(sessionObj.id, "=", e.cast(e.uuid, sessionId)),
+            set: partialSession,
+          })
+        );
+
+        await query.run(client);
+      },
       getKey: async (keyId) => {
-        return null;
+        const query = e.select(Key, (keyObj: { key_id: string }) => ({
+          ...Key["*"],
+          filter_single: e.op(keyObj.key_id, "=", e.cast(e.uuid, keyId)),
+        }));
+
+        return await query.run(client);
       },
       getKeysByUserId: async (userId) => {
-        return [];
+        const query = e.select(Key, (keyObj: { user: { id: string } }) => ({
+          ...Key["*"],
+          filter: e.op(keyObj.user.id, "=", e.cast(e.uuid, userId)),
+        }));
+
+        return await query.run(client);
       },
-      setKey: async (key) => {},
-      deleteKey: async (keyId) => {},
-      deleteKeysByUserId: async (userId) => {},
-      updateKey: async (keyId, partialKey) => {},
+      setKey: async (key) => {
+        try {
+          const { user_id, id: key_id, ...keyWithoutUserId } = key;
+          const query = e.insert(Key, {
+            key_id,
+            ...keyWithoutUserId,
+            user: e.select(User, (userObj: GlobalDatabaseUserAttributes) => ({
+              filter_single: e.op(userObj.id, "=", e.cast(e.uuid, user_id)),
+            })),
+          });
+          await query.run(client);
+        } catch (error) {
+          type ErrorCasting = { message?: string };
+          // Catch duplicate key id error
+          if (
+            (error as ErrorCasting).message &&
+            (error as ErrorCasting).message?.includes(
+              `${modelNames.key}: key_id`
+            )
+          ) {
+            throw new LuciaError("AUTH_DUPLICATE_KEY_ID");
+          }
+
+          // Catch invalid user id errors
+          if (
+            (error as ErrorCasting).message &&
+            (error as ErrorCasting).message?.includes(
+              `missing value for required link 'user'`
+            )
+          ) {
+            throw new LuciaError("AUTH_INVALID_USER_ID");
+          }
+
+          throw error;
+        }
+      },
+      deleteKey: async (keyId) => {
+        const query = e.delete(Key, (keyObj: { key_id: string }) => ({
+          filter_single: e.op(keyObj.key_id, "=", e.cast(e.uuid, keyId)),
+        }));
+
+        await query.run(client);
+      },
+      deleteKeysByUserId: async (userId) => {
+        const query = e.delete(Key, (keyObj: { user: { id: string } }) => ({
+          filter: e.op(keyObj.user.id, "=", e.cast(e.uuid, userId)),
+        }));
+
+        await query.run(client);
+      },
+      updateKey: async (keyId, partialKey) => {
+        const query = e.update(Key, (keyObj: { key_id: string }) => ({
+          filter_single: e.op(keyObj.key_id, "=", e.cast(e.uuid, keyId)),
+          set: partialKey,
+        }));
+
+        await query.run(client);
+      },
       getSessionAndUser: async (sessionId) => {
-        return [null, null];
+        if (Session === null) {
+          throw new Error("Session table not defined");
+        }
+
+        const query = e.select(
+          Session,
+          (sessionObj: GlobalDatabaseSessionAttributes) => ({
+            ...Session["*"],
+            user: {
+              ...User["*"],
+            },
+            filter_single: e.op(sessionObj.id, "=", e.cast(e.uuid, sessionId)),
+          })
+        );
+
+        const result = await query.run(client);
+
+        if (!result) return [null, null];
+
+        const { user, ...session } = result;
+        return [session, user];
       },
-      //   deleteUser: async (userId) => {
-      //     await pool.query(
-      //       `DELETE FROM ${ESCAPED_USER_TABLE_NAME} WHERE id = $1`,
-      //       [userId]
-      //     );
-      //   },
-      //   updateUser: async (userId, partialUser) => {
-      //     const [fields, values, args] = helper(partialUser);
-      //     await pool.query(
-      //       `UPDATE ${ESCAPED_USER_TABLE_NAME} SET ${getSetArgs(
-      //         fields,
-      //         values
-      //       )} WHERE id = $${fields.length + 1}`,
-      //       [...args, userId]
-      //     );
-      //   },
-
-      //   getSession: async (sessionId) => {
-      //     if (!ESCAPED_SESSION_TABLE_NAME) {
-      //       throw new Error("Session table not defined");
-      //     }
-      //     const result = await get<DatabaseSession>(
-      //       pool.query(
-      //         `SELECT * FROM ${ESCAPED_SESSION_TABLE_NAME} WHERE id = $1`,
-      //         [sessionId]
-      //       )
-      //     );
-      //     return result ? transformDatabaseSession(result) : null;
-      //   },
-      //   getSessionsByUserId: async (userId) => {
-      //     if (!ESCAPED_SESSION_TABLE_NAME) {
-      //       throw new Error("Session table not defined");
-      //     }
-      //     const result = await getAll<DatabaseSession>(
-      //       pool.query(
-      //         `SELECT * FROM ${ESCAPED_SESSION_TABLE_NAME} WHERE user_id = $1`,
-      //         [userId]
-      //       )
-      //     );
-      //     return result.map((val) => transformDatabaseSession(val));
-      //   },
-      //   setSession: async (session) => {
-      //     if (!ESCAPED_SESSION_TABLE_NAME) {
-      //       throw new Error("Session table not defined");
-      //     }
-      //     try {
-      //       const [fields, values, args] = helper(session);
-      //       await pool.query(
-      //         `INSERT INTO ${ESCAPED_SESSION_TABLE_NAME} ( ${fields} ) VALUES ( ${values} )`,
-      //         args
-      //       );
-      //     } catch (e) {
-      //       const error = e as Partial<DatabaseError>;
-      //       if (
-      //         error.code === "23503" &&
-      //         error.detail?.includes("Key (user_id)")
-      //       ) {
-      //         throw new LuciaError("AUTH_INVALID_USER_ID");
-      //       }
-      //       throw e;
-      //     }
-      //   },
-      //   deleteSession: async (sessionId) => {
-      //     if (!ESCAPED_SESSION_TABLE_NAME) {
-      //       throw new Error("Session table not defined");
-      //     }
-      //     await pool.query(
-      //       `DELETE FROM ${ESCAPED_SESSION_TABLE_NAME} WHERE id = $1`,
-      //       [sessionId]
-      //     );
-      //   },
-      //   deleteSessionsByUserId: async (userId) => {
-      //     if (!ESCAPED_SESSION_TABLE_NAME) {
-      //       throw new Error("Session table not defined");
-      //     }
-      //     await pool.query(
-      //       `DELETE FROM ${ESCAPED_SESSION_TABLE_NAME} WHERE user_id = $1`,
-      //       [userId]
-      //     );
-      //   },
-      //   updateSession: async (sessionId, partialSession) => {
-      //     if (!ESCAPED_SESSION_TABLE_NAME) {
-      //       throw new Error("Session table not defined");
-      //     }
-      //     const [fields, values, args] = helper(partialSession);
-      //     await pool.query(
-      //       `UPDATE ${ESCAPED_SESSION_TABLE_NAME} SET ${getSetArgs(
-      //         fields,
-      //         values
-      //       )} WHERE id = $${fields.length + 1}`,
-      //       [...args, sessionId]
-      //     );
-      //   },
-
-      //   getKey: async (keyId) => {
-      //     const result = await get(
-      //       pool.query<KeySchema>(
-      //         `SELECT * FROM ${ESCAPED_KEY_TABLE_NAME} WHERE id = $1`,
-      //         [keyId]
-      //       )
-      //     );
-      //     return result;
-      //   },
-      //   getKeysByUserId: async (userId) => {
-      //     const result = getAll<KeySchema>(
-      //       pool.query(
-      //         `SELECT * FROM ${ESCAPED_KEY_TABLE_NAME} WHERE user_id = $1`,
-      //         [userId]
-      //       )
-      //     );
-      //     return result;
-      //   },
-      //   setKey: async (key) => {
-      //     try {
-      //       const [fields, values, args] = helper(key);
-      //       await pool.query(
-      //         `INSERT INTO ${ESCAPED_KEY_TABLE_NAME} ( ${fields} ) VALUES ( ${values} )`,
-      //         args
-      //       );
-      //     } catch (e) {
-      //       const error = e as Partial<DatabaseError>;
-      //       if (
-      //         error.code === "23503" &&
-      //         error.detail?.includes("Key (user_id)")
-      //       ) {
-      //         throw new LuciaError("AUTH_INVALID_USER_ID");
-      //       }
-      //       if (error.code === "23505" && error.detail?.includes("Key (id)")) {
-      //         throw new LuciaError("AUTH_DUPLICATE_KEY_ID");
-      //       }
-      //       throw e;
-      //     }
-      //   },
-      //   deleteKey: async (keyId) => {
-      //     await pool.query(
-      //       `DELETE FROM ${ESCAPED_KEY_TABLE_NAME} WHERE id = $1`,
-      //       [keyId]
-      //     );
-      //   },
-      //   deleteKeysByUserId: async (userId) => {
-      //     await pool.query(
-      //       `DELETE FROM ${ESCAPED_KEY_TABLE_NAME} WHERE user_id = $1`,
-      //       [userId]
-      //     );
-      //   },
-      //   updateKey: async (keyId, partialKey) => {
-      //     const [fields, values, args] = helper(partialKey);
-      //     await pool.query(
-      //       `UPDATE ${ESCAPED_KEY_TABLE_NAME} SET ${getSetArgs(
-      //         fields,
-      //         values
-      //       )} WHERE id = $${fields.length + 1}`,
-      //       [...args, keyId]
-      //     );
-      //   },
-
-      //   getSessionAndUser: async (sessionId) => {
-      //     if (!ESCAPED_SESSION_TABLE_NAME) {
-      //       throw new Error("Session table not defined");
-      //     }
-      //     const getSessionPromise = get(
-      //       pool.query<DatabaseSession>(
-      //         `SELECT * FROM ${ESCAPED_SESSION_TABLE_NAME} WHERE id = $1`,
-      //         [sessionId]
-      //       )
-      //     );
-      //     const getUserFromJoinPromise = get(
-      //       pool.query<
-      //         UserSchema & {
-      //           __session_id: string;
-      //         }
-      //       >(
-      //         `SELECT ${ESCAPED_USER_TABLE_NAME}.*, ${ESCAPED_SESSION_TABLE_NAME}.id as __session_id FROM ${ESCAPED_SESSION_TABLE_NAME} INNER JOIN ${ESCAPED_USER_TABLE_NAME} ON ${ESCAPED_USER_TABLE_NAME}.id = ${ESCAPED_SESSION_TABLE_NAME}.user_id WHERE ${ESCAPED_SESSION_TABLE_NAME}.id = $1`,
-      //         [sessionId]
-      //       )
-      //     );
-      //     const [sessionResult, userFromJoinResult] = await Promise.all([
-      //       getSessionPromise,
-      //       getUserFromJoinPromise,
-      //     ]);
-      //     if (!sessionResult || !userFromJoinResult) return [null, null];
-      //     const { __session_id: _, ...userResult } = userFromJoinResult;
-      //     return [transformDatabaseSession(sessionResult), userResult];
-      //   },
     };
   };
 };
